edb7fc2e5d327215ad2800840a93c0428e4098c5
Daniel Cohen <dcoh@umich.edu>
2020-11-24 20:30:01 -0500

    created a blueprint in comments for the beginning of our project

--- final.py
+++ final.py
@@ -1,1 +1,17 @@
 #does this work
+#question we are answering: How does an NBA team’s performance affect local businesses?
+#first function will access the API's/website
+#second function will access and store at least 100 items in your database from each API/website in at least one table per API/website. 
+# For at least one API you must have two tables that share a key
+# You must not have duplicate data in your database!  Do not split data from one table into two! 
+# Also, there should be only one database! 
+#third function will limit how much data you store from an API into the database each time you execute your code to 25 or fewer items 
+# The data must be stored in a SQLite database. This meansthat you must run the code that stores the data multiple times 
+# to gather at least 100 items total without duplicating existing data or changing it.
+#then we process the data- 3 functions
+#first function is selecting some data from all of the tables in your database and calculate something from that data
+#example for first function: Calculating # of hm items occur on a particular day of the week
+#  or the average of the number of items per day.  
+#second function: You must do at least one database jointo select your data
+#third function: Write out the calculated data to a file as text
+#Next step: at least 3 visualizations, different from what we did in lecture, maybe 2 more visualizations for extra credit
