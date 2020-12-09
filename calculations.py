33be8fcd501f9d8c27e826a6d7db0272aa8079ce
kegould <71288179+kegould@users.noreply.github.com>
2020-12-08 22:41:37 -0600

    Write to a file

--- calculations.py
+++ calculations.py
@@ -4,12 +4,13 @@ import os
 import requests
 import math
 def table_join():
-    conn= sqlite3.connect('/Users/saracohen/Downloads/KateDannyBradleyFinalProject/Final.db')
+    conn= sqlite3.connect('/Users/kategould/documents/KateDannyBradleyFinalProject/Final.db')
     cur = conn.cursor() 
     cur.execute("SELECT NBA_Stats.Player, NBA_Stats.PPG, NBA_Season.Home_Score, NBA_Season.Home FROM NBA_Season JOIN NBA_Stats ON NBA_Season.Home=NBA_Stats.Team LIMIT 25")
     x= cur.fetchall()
     y= list(x)
     final_lst= []
+    fileobj=open('Calculations.txt','w')
     for i in y:
         calculated_num= i[1]
         calculated_num1= i[2]
@@ -17,7 +18,9 @@ def table_join():
         final_var= calculated_num2 * 100
         final_var1= math.floor(final_var)
         final_lst.append("Player's Percentage Points of their Team: " + str(i[0]) + " " + str(final_var1) + '%')
+        fileobj.write("Player's Percentage Points of their Team: " + str(i[0]) + " " + str(final_var1) + '%')
     return y
+    fileobj.close()
 table_join()
     
     
